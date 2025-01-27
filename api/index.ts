eb6e1d5da9452453c54e0f5a2ffcbbb94352ca3d
Vlad Yuskovich <vladyuskovich@gmail.com>
2025-01-27 18:26:34 +0300

    upd: routes

--- api/index.ts
+++ api/index.ts
@@ -3,15 +3,7 @@ import cookieParser from 'cookie-parser'
 import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
 import { errorMiddleware } from './middlewares'
-import { authMiddleware } from './middlewares/auth.middleware'
-import {
-  authRouter,
-  cashAccountRouter,
-  categoryRouter,
-  operationRouter,
-  userRouter
-} from './modules'
-import { dashboardRouter } from './modules/dashboard'
+import { authRouter } from './modules'
 import { envVariable, onApplicationBootstrap } from './utils'
 
 const app: Application = express()
@@ -25,12 +17,12 @@ app.use(bodyParser.json({ limit: '6mb' }))
 app.use(bodyParser.urlencoded({ limit: '6mb', extended: true }))
 app.use(cookieParser(envVariable('COOKIE_SECRET', { isRequired: true })))
 
-app.use('/api/users', authMiddleware, userRouter)
-app.use('/api/category', authMiddleware, categoryRouter)
-app.use('/api/regular-operation', authMiddleware, operationRouter)
-app.use('/api/cash-account', authMiddleware, cashAccountRouter)
-app.use('/api/operation', authMiddleware, operationRouter)
-app.use('/api/dashboard', authMiddleware, dashboardRouter)
+// app.use('/api/users', authMiddleware, userRouter)
+// app.use('/api/category', authMiddleware, categoryRouter)
+// app.use('/api/regular-operation', authMiddleware, operationRouter)
+// app.use('/api/cash-account', authMiddleware, cashAccountRouter)
+// app.use('/api/operation', authMiddleware, operationRouter)
+// app.use('/api/dashboard', authMiddleware, dashboardRouter)
 app.use('/api/auth', authRouter)
 
 const PORT = envVariable<number>('PORT', { isRequired: true })
